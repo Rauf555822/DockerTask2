755663467da6a9014d6a96bf745a92efe09cc355
Rauf555822 <raufabdurhimov@gmail.com>
2025-06-29 14:37:09 +0400

    Repository annotation add

--- src/main/java/org/example/dockertask2/repository/GunRepository.java
+++ src/main/java/org/example/dockertask2/repository/GunRepository.java
@@ -2,6 +2,8 @@ package org.example.dockertask2.repository;
 
 import org.example.dockertask2.model.Gun;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GunRepository extends MongoRepository<Gun, String> {
 }
